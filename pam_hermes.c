c61b37ccb84f7dbc63b2d0cfcd91ab93aa306e3c
Florian MARGAINE <florian@margaine.com>
2015-09-09 13:43:21 +0200

    Simplifies the required authentications

--- pam_hermes.c
+++ pam_hermes.c
@@ -30,16 +30,6 @@ static bool is_hermes_device(const char*);
 static bool can_login(struct hermes_device*, const char*);
 static bool is_authenticated(const char*);
 
-PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv)
-{
-	return PAM_SUCCESS;
-}
-
-PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
-{
-	return PAM_SUCCESS;
-}
-
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags,int argc, const char **argv)
 {
 	char *user;
